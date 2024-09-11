3efcc608439e0f2d1dc54e9f458c2f6d1c56eafb
Sooslick <lexapyc@mail.ru>
2024-09-11 13:05:46 +0300

    improved seed tester

--- src/ru/sooslick/scpcb/SeedTester.java
+++ src/ru/sooslick/scpcb/SeedTester.java
@@ -7,31 +7,93 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SeedTester {
-    public static void main(String[] args) throws IOException {
-        int baseSeed = 56141;
+    public final static String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    public final static String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public final static String NUMBERS = "0123456789";
 
-        // EXTREMELY UNOPTIMISED CYCLE.
-        // OK for small alphabets or seed length < 8 characters, otherwise it will last forever
-        // TODO introduce new bruteforce function with binary math that optimized for seed search
+    int baseSeed = 7839;
+    boolean[] controlBytes = new boolean[32];
 
-        int[] charPositions = {2, 2, 2, 2, 2, 2, 26, 0, 1, 0};    // last saved word
-        char[] seed;
+    String dictionary = UPPERCASE + LOWERCASE;
+    int dictionaryLength = dictionary.length();
+    int length = 8;
 
-        // max possible seed length: 15;
-//        BruteForce bf = new BruteForce(BruteForce.ALL_LETTERS, 2, 6);
-        BruteForce bf = new BruteForce(BruteForce.CUSTOM, 10, 10, charPositions);
-//        SeedPrinter p = new SeedPrinterImpl();
-        SeedPrinter p = new DictionarySeedPrinter();
+    int[] chars;
+    int[] state;
+    int[] result;
 
-        do {
-            seed = bf.next();
-            if (SeedGenerator.generateSeedNumber(seed) == baseSeed) {
-                p.print(new String(seed));
-            }
-        } while (!bf.isFinished());
+    SeedPrinter printer = new DictionarySeedPrinter();
+
+    public static void main(String[] args) {
+        SeedTester seedTester = new SeedTester();
+        if (args.length > 0)
+            seedTester.baseSeed = Integer.parseInt(args[0]);
+        if (args.length > 1)
+            seedTester.dictionary = args[1];
+        seedTester.search();
     }
 
-    private static interface SeedPrinter {
+    public void search() {
+        calcControlBytes();
+        initState();
+        calcState(0);
+    }
+
+    private void calcControlBytes() {
+        for (int i = 0; i < 32; i++)
+            controlBytes[i] = testByte(baseSeed, i);
+    }
+
+    private boolean testByte(int victim, int position) {
+        return (victim & 1 << position) >> position == 1;
+    }
+
+    private void initState() {
+        chars = new int[dictionaryLength];
+        for (int i = 0; i < dictionaryLength; i++)
+            chars[i] = dictionary.charAt(i);
+
+        state = new int[length];
+        result = new int[length];
+    }
+
+    private void calcState(int pointer) {
+        if (pointer >= length) {
+            if (result[pointer - 1] == baseSeed)
+                printSeed(printer);
+            return;
+        }
+
+        int prevResult = pointer == 0 ? 0 : result[pointer - 1];
+        boolean controlByte = controlBytes[pointer];
+        while (updateResult(pointer, prevResult)) {
+            if (testByte(result[pointer], pointer) == controlByte)
+                calcState(pointer + 1);
+            state[pointer]++;
+        }
+    }
+
+    private boolean updateResult(int pointer, int prevResult) {
+        if (state[pointer] >= dictionaryLength) {
+            state[pointer] = 0;
+            return false;
+        }
+
+        int currentChar = chars[state[pointer]];
+        result[pointer] = prevResult ^ (currentChar << pointer);
+        return true;
+    }
+
+    private void printSeed(SeedPrinter printer) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++)
+            sb.append((char) chars[state[i]]);
+        printer.print(sb.toString());
+    }
+
+    ///////////////////////////////
+
+    private interface SeedPrinter {
         void print(String seed);
     }
 
@@ -49,14 +111,19 @@ public class SeedTester {
     }
 
     private static class DictionarySeedPrinter implements SeedPrinter {
-        private final List<String> dictionary = new ArrayList<>(
-                Files.readAllLines(Paths.get("WL_DICT.txt")));
+        private final List<String> dictionary;
 
-        private DictionarySeedPrinter() throws IOException {
+        private DictionarySeedPrinter() {
+            try {
+                dictionary = new ArrayList<>(
+                        Files.readAllLines(Paths.get("WL_DICT.txt")));
+            } catch (IOException e) {
+                throw new RuntimeException("bruh gimme acces to dictionary", e);
+            }
         }
 
         public void print(String seed) {
-            if (dictionary.contains(seed.toUpperCase().replaceAll("[A-Z]", "")))
+            if (dictionary.contains(seed.toUpperCase().replaceAll("[^A-Z]", "")))
                 System.out.println(seed);
         }
     }
