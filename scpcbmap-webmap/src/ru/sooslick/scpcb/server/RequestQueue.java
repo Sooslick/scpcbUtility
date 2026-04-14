fa6a8c74a444e67e18be0864b482a0c60fb1a47b
Sooslick <lexapyc@mail.ru>
2026-04-14 15:26:41 +0300

    rebased server to maven branch

--- scpcbmap-webmap/src/ru/sooslick/scpcb/server/RequestQueue.java
+++ scpcbmap-webmap/src/ru/sooslick/scpcb/server/RequestQueue.java
@@ -2,13 +2,15 @@ package ru.sooslick.scpcb.server;
 
 import ru.sooslick.scpcb.MapExplorer;
 import ru.sooslick.scpcb.SeedGenerator;
+import ru.sooslick.scpcb.map.Map;
 
 import java.util.function.Function;
 
 public class RequestQueue {
 
     public static synchronized String requestMap(String seed, Function<String, Integer> method) {
-        MapExplorer pf = SeedGenerator.generateMap(seed, method);
+        Map map = SeedGenerator.generateMap(seed, method);
+        MapExplorer pf = new MapExplorer(seed, method.apply(seed), map);
         return pf.exportJson();
     }
 }
